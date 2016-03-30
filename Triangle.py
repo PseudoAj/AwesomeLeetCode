e05e4e76773efe43a7bffc810d1880de3583c165
Ajay Krishna Teja Kavuri <PseudoAj@users.noreply.github.com>
2016-03-30 19:27:33 -0400

    First problem

---
+++ Triangle.py
@@ -0,0 +1,54 @@
+class Solution(object):
+    def minimumTotal(self, triangle):
+        """
+        :type triangle: List[List[int]]
+        :rtype: int
+        """
+        sum=0
+        pIdx=0
+        for row in triangle:
+            i=0
+            if(i==0):
+                element=self.getMin(row,0)
+            else:
+                for i in xrange(len(row)):
+                    temp=self.getMin(row,j)
+                    if self.isNgh(pIdx,row.index(temp)):
+                        element=temp
+                        break
+            
+            sum+=element
+        return sum
+        
+        
+    def getMin(self,row,i):
+        a=self.msort(row)
+        return a[i]
+    
+    def msort(self,x):
+        result = []
+        if len(x) < 20:
+            return sorted(x)
+        mid = int(len(x)/2)
+        y = self.msort(x[:mid])
+        z = self.msort(x[mid:])
+        i = 0
+        j = 0
+        while i < len(y) and j < len(z):
+                if y[i] > z[j]:
+                    result.append(z[j])
+                    j += 1
+                else:
+                    result.append(y[i])
+                    i += 1
+        result += y[i:]
+        result += z[j:]
+        return result
+    
+    def isNgh(self,i,j):
+        if (j-1==0):
+            return True
+        elif(j-1==1):
+            return True
+        else:
+            return False
